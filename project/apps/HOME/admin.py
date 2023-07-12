363542f3e1fae5c41847c2e5e989c52c1a9efc49
Federico Tempo <federicotempo98@gmail.com>
2023-07-12 19:46:05 -0300

    add: modelos registrados

--- project/apps/HOME/admin.py
+++ project/apps/HOME/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+admin.site.register(models.Cliente)
+admin.site.register(models.Dias)
+admin.site.register(models.Vehiculo)
--- project/apps/HOME/models.py
+++ project/apps/HOME/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Vehiculo(models.Model):
+    nombre = models.CharField(max_length = 100)
+
+class Dias(models.Model):
+    cantidad_dias = models.IntegerField()
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length = 100)
+    apellido = models.CharField(max_length = 100)
+    edad = models.IntegerField()
+    documento = models.IntegerField()
+    vehiculo_id = models.ForeignKey(Vehiculo, on_delete = models.SET_NULL, null = True, blank = True)
+    dias_id = models.ForeignKey(Dias, on_delete = models.SET_NULL, null = True, blank = True)
